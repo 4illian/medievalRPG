0fce443d8ab12fbdc773b163f8a69a1e73552c6b
4illian <killian.occelli@epitech.eu>
2022-07-19 16:59:01 +0200

    Select player front

--- mediaval/config/routes.rb
+++ mediaval/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :games
   devise_for :users
+  get "user_creation", to: "user_creation#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
