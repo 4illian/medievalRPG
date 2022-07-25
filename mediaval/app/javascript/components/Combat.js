cce2032ba7acd8a3f08b01c460f929efbab1eedd
4illian <killian.occelli@epitech.eu>
2022-07-25 16:18:52 +0200

    first page game

--- mediaval/app/javascript/components/Combat.js
+++ mediaval/app/javascript/components/Combat.js
@@ -6,6 +6,8 @@ function Combat() {
     // player = 0
     // IA = 1
     let currentPlayerTurn = 0
+    let countTurnPlayed = 0
+    let isBoss = false
 
     let player = {
         "life": 0,
@@ -39,30 +41,24 @@ function Combat() {
 
 
     function TurnBasedCheck() {
-        if (currentPlayerTurn === 0) PlayerTurnProcess()
+        if (currentPlayerTurn === 0) PlayerDisplayInterface()
         else IATurnProcess()
     }
 
-    function PlayerTurnProcess() {
-        // print interface with capacity
-        PlayerDisplayInterface()
-
-    }
+    // function PlayerDisplayInterface() {
+    //     // print interface with capacity
+    //     PlayerDisplayInterface()
+    //
+    // }
 
     function PlayerAttack() {
         if (currentAttack.type === 0) { // is ATTACK
 
             // set buff player
-            let buffDamage = 0
-            for (var i = 0; i < currentPlayerBuff.buff.length; i++) {
-                buffDamage += currentPlayerBuff.buff[i].buff
-            }
+            let buffDamage = getCurrentPlayerBuff()
 
             // set protection ennemy
-            let buffEnnemyDefense = 0
-            for (var j = 0; j < currentEnnemyBuff.protection.length; i++) {
-                buffEnnemyDefense += currentEnnemyBuff.protection[i].protection
-            }
+            let buffEnnemyDefense = getCurrentEnnemyProtection()
 
             // set damage
             ennemy.life = (ennemy.life + ennemy.defense + buffEnnemyDefense)
@@ -89,7 +85,8 @@ function Combat() {
             BuffUpdate()
 
             // set current attack buff
-            currentPlayerBuff.protection = currentAttack.protection
+            let json = JSON(currentAttack.protectionDuration, currentAttack.protection)
+            currentPlayerBuff.protection.push(json)
         }
 
 
@@ -98,7 +95,8 @@ function Combat() {
             BuffUpdate()
 
             // set current attack buff
-            player.attack = player.attack + currentAttack.buff
+            let json = JSON(currentAttack.buffDuration, currentAttack.buff)
+            currentPlayerBuff.buff.push(json)
         }
 
         CombatEndCheck()
@@ -127,19 +125,72 @@ function Combat() {
     }
 
 
-    function EnnemyAttack() {
+    function EnnemyAttack(attack) {
+        currentAttack = attack
+        // display dialog " Ennemy attack ....."
+
+
+        if (currentAttack.type === 0) { // is ATTACK
+
+            // set buff player
+            let buffDamage = getCurrentEnnemyBuff()
+
+            // set protection ennemy
+            let buffPlayerDefense = getCurrentPlayerProtection()
+
+            // set damage
+            player.life = (player.life + player.defense + buffPlayerDefense)
+                - (currentAttack.damage + ennemy.attack + buffDamage)
+
+            // update buff before set new buffs
+            BuffUpdate()
+
+            // set buff attack
+            if (currentAttack.protection > 0) {
+                let json = JSON(currentAttack.protectionDuration, currentAttack.protection)
+                currentEnnemyBuff.protection.push(json)
+            }
+
+            if (currentAttack.buff > 0) {
+                let json = JSON(currentAttack.buffDuration, currentAttack.buff)
+                currentEnnemyBuff.buff.push(json)
+            }
+        }
+
+
+        if (currentAttack.type === 1) { // is PROTECTION
+            // update buff before set new buffs
+            BuffUpdate()
+
+            // set current attack buff
+            let json = JSON(currentAttack.protectionDuration, currentAttack.protection)
+            currentEnnemyBuff.protection.push(json)
+        }
+
+
+        if (currentAttack.type === 2) {  // is BUFF
+            // update buff before set new buffs
+            BuffUpdate()
+
+            // set current attack buff
+            let json = JSON(currentAttack.buffDuration, currentAttack.buff)
+            currentEnnemyBuff.buff.push(json)
+        }
 
         CombatEndCheck()
     }
 
 
-    function IADisplayInterface() {
+    function PrintEnnemyTurnText() {
         // display "adversaire en cours de reflexion ..."
-        return {}
+        // need await 3 secondes
     }
 
     function PlayerDisplayInterface() {
         // display capacity && inventory
+
+
+
 
         // onclick capacity ->
         // print attack used to ennemy in chat
@@ -149,12 +200,126 @@ function Combat() {
 
     function IATurnProcess() {
 
-        IADisplayInterface()
+        PrintEnnemyTurnText()
 
-        // PROCESS WITH FIX IA AND
+        // check if one of the abilities can end the fight
+        for (var i = 0; i < ennemy.capacity.size; i++) {
+            if ((getCurrentPlayerProtection() + player.defense + player.life)
+                <= (getCurrentEnnemyBuff() + ennemy.attack + ennemy.capacity[i].damage)) {
+                // play this capacity
+            }
+        }
+
+        // if not
+        if (!isBoss) {
+
+            let jet = getRandomInt(11) // 0 - 10
+
+            if (countTurnPlayed < 3) { // favorise le buff
+                if (jet > 4) {
+                    if (jet > 7) {
+                        // buff protec
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].protection > 0) {
+                                EnnemyAttack(ennemy.capacity[i])
+                            }
+                        }
+                    } else {
+                        // buff attack
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].buff > 0) {
+                                EnnemyAttack(ennemy.capacity[i])
+                            }
+                        }
+                    }
+                } else {
+                    //attack
+                    var attackCapacities = []
+                    for (var i = 0; ennemy.capacity.length; i++) {
+                        if (ennemy.capacity[i].attack > 0) {
+                            attackCapacities.push(i)
+                        }
+                    }
+                    if (attackCapacities.length > 0) {
+                        if (jet > 2) {
+                            EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                        } else {
+                            EnnemyAttack(ennemy.capacity[attackCapacities[1]])
+                        }
+                    } else {
+                        EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                    }
+                }
+
+            } else { // classic process / 0-5 attack / 6-8 buff / 9-10 protec
+
+                if (ennemy.life <= 35) {
+                    // protec or attack
+
+                    if (jet < 6) {
+                        // launch attack
+                        var attackCapacities = []
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].attack > 0) {
+                                attackCapacities.push(i)
+                            }
+                        }
+                        if (attackCapacities.length > 0) {
+                            if (jet > 3) {
+                                EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                            } else {
+                                EnnemyAttack(ennemy.capacity[attackCapacities[1]])
+                            }
+                        } else {
+                            EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                        }
+                    } else {
+                        // launch protection
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].protection > 0) {
+                                EnnemyAttack(ennemy.capacity[i])
+                            }
+                        }
+                    }
+                } else {
+                    // attack or buff or protec
+                    if (jet <= 5) {
+                        // attack
+                        var attackCapacities = []
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].attack > 0) {
+                                attackCapacities.push(i)
+                            }
+                        }
+                        if (attackCapacities.length > 0) {
+                            if (jet > 2) {
+                                EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                            } else {
+                                EnnemyAttack(ennemy.capacity[attackCapacities[1]])
+                            }
+                        } else {
+                            EnnemyAttack(ennemy.capacity[attackCapacities[0]])
+                        }
+                    } else if (jet <= 8 && jet > 5) {
+                        // buff
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].buff > 0) {
+                                EnnemyAttack(ennemy.capacity[i])
+                            }
+                        }
+                    } else {
+                        // protec
+                        for (var i = 0; ennemy.capacity.length; i++) {
+                            if (ennemy.capacity[i].protection > 0) {
+                                EnnemyAttack(ennemy.capacity[i])
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         // remove life in current player in ennemy attack
-        EnnemyAttack()
     }
 
 
@@ -175,6 +340,7 @@ function Combat() {
                 EndCombatVictory()
             } else {
                 currentPlayerTurn++
+                countTurnPlayed++
                 IATurnProcess()
             }
         } else {
@@ -183,27 +349,65 @@ function Combat() {
                 EndCombatDefeat()
             } else {
                 currentPlayerTurn--
+                countTurnPlayed++
                 PlayerTurnProcess()
             }
         }
     }
 
     function EndCombatVictory() {
+        console.log("PLAYER VICTORY")
         // display message victory and leave combat
     }
 
 
     function EndCombatDefeat() {
+        console.log("PLAYER DEFEAT")
         // display message defeat and retry
     }
 
+    function getCurrentEnnemyBuff() {
+        var buffDamage = 0
+        for (var i = 0; i < currentEnnemyBuff.buff.length; i++) {
+            buffDamage += currentEnnemyBuff.buff[i].buff
+        }
+        return buffDamage
+    }
+
+    function getCurrentEnnemyProtection() {
+        var buffProtection = 0
+        for (var i = 0; i < currentEnnemyBuff.protection.length; i++) {
+            buffProtection += currentEnnemyBuff.protection[i].protection
+        }
+        return buffProtection
+    }
+
+    function getCurrentPlayerBuff() {
+        var buffDamage = 0
+        for (var i = 0; i < currentPlayerBuff.buff.length; i++) {
+            buffDamage += currentPlayerBuff.buff[i].buff
+        }
+        return buffDamage
+    }
+
+    function getCurrentPlayerProtection() {
+        var buffProtection = 0
+        for (var i = 0; i < currentPlayerBuff.protection.length; i++) {
+            buffProtection += currentPlayerBuff.protection[i].protection
+        }
+        return buffProtection
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
 
     // start combat process
     TurnBasedCheck()
 
     return (
         <div>
-            dede
+            dedes
             {/*<Dialog name={"Raoul"} content={"yoooo je suis raoul le caoul"} />*/}
 
             {/*<div className={"player_game_start"} />*/}
